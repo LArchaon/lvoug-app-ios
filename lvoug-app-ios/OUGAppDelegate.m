d1314b6301c3f60ca31534f2b7b3751445d7da4e
Данил <bekasik@gmail.com>
2014-02-09 17:32:19 +0200

    Link to twitter added

--- lvoug-app-ios/OUGAppDelegate.m
+++ lvoug-app-ios/OUGAppDelegate.m
@@ -9,13 +9,14 @@
     MFSideMenuContainerViewController *container = (MFSideMenuContainerViewController *)self.window.rootViewController;
     UINavigationController *navigationController = [storyboard instantiateViewControllerWithIdentifier:@"navigationController"];
 
+
     UIViewController *leftSideMenuViewController = [storyboard instantiateViewControllerWithIdentifier:@"leftSideMenuViewController"];
    // UIViewController *rightSideMenuViewController = [storyboard instantiateViewControllerWithIdentifier:@"rightSideMenuViewController"];
     
     [container setLeftMenuViewController:leftSideMenuViewController];
     //[container setRightMenuViewController:rightSideMenuViewController];
     [container setCenterViewController:navigationController];
-   
+    
     return YES;
 }
 @end
