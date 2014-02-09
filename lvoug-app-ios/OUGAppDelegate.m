bea61d2218ff12227e264a85607b8e8a5dc81ecd
Данил <bekasik@gmail.com>
2014-02-09 16:25:58 +0200

    Left navigation bar added

--- lvoug-app-ios/OUGAppDelegate.m
+++ lvoug-app-ios/OUGAppDelegate.m
@@ -1,9 +1,21 @@
 #import "OUGAppDelegate.h"
+#import "MFSideMenuContainerViewController.h"
 
 @implementation OUGAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Home" bundle:[NSBundle mainBundle]];
+    MFSideMenuContainerViewController *container = (MFSideMenuContainerViewController *)self.window.rootViewController;
+    UINavigationController *navigationController = [storyboard instantiateViewControllerWithIdentifier:@"navigationController"];
+
+    UIViewController *leftSideMenuViewController = [storyboard instantiateViewControllerWithIdentifier:@"leftSideMenuViewController"];
+   // UIViewController *rightSideMenuViewController = [storyboard instantiateViewControllerWithIdentifier:@"rightSideMenuViewController"];
+    
+    [container setLeftMenuViewController:leftSideMenuViewController];
+    //[container setRightMenuViewController:rightSideMenuViewController];
+    [container setCenterViewController:navigationController];
+   
     return YES;
 }
 @end
