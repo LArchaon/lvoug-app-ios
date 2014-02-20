239759216ed26b6e16fdf2daa5d52509a97262a0
Данил <bekasik@gmail.com>
2014-02-20 23:16:07 +0200

    Show event venue on map

--- lvoug-app-ios/EventVC.m
+++ lvoug-app-ios/EventVC.m
@@ -1,5 +1,6 @@
 #import "EventVC.h"
 #import "APIClient.h"
+#define METERS_PER_MILE 1609.344
 
 @interface EventVC ()
 
@@ -15,6 +16,31 @@
     
     self.eventTitle.text = [event objectForKey:@"title"];
     self.eventText.text = [event objectForKey:@"description"];
+    
+    id latitude = [event objectForKey:@"address_latitude"];
+    id longitude = [event objectForKey:@"address_longitude"];
+    [self initMapWithLatitude:[latitude doubleValue] andWithLongitude:[longitude doubleValue]];
+
+}
+
+-(void)initMapWithLatitude:(double)latitude andWithLongitude:(double)longitude
+{
+    CLLocationCoordinate2D annotationCoord;
+    annotationCoord.latitude = latitude;
+    annotationCoord.longitude = longitude;
+    
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = latitude;
+    zoomLocation.longitude = longitude;
+    
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+    
+    MKPointAnnotation *annotationPoint = [[MKPointAnnotation alloc] init];
+    annotationPoint.coordinate = annotationCoord;
+    annotationPoint.title = self.eventTitle.text;
+    
+    [self.eventMap setRegion:viewRegion animated:YES];
+    [self.eventMap addAnnotation:annotationPoint];
 }
 
 -(void)setEvent:(NSString *)eventId
